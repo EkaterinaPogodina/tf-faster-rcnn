3657941ac728d6a95a2d9070026e9672b1d51dc5
Ekaterina Pogodina <pogodina@phystech.edu>
2019-07-18 16:57:56 +0300

    try

--- lib/nets/resnet_v1.py
+++ lib/nets/resnet_v1.py
@@ -105,6 +105,14 @@ class resnetv1(Network):
                                            reuse=reuse,
                                            scope=self._scope)
 
+      with slim.arg_scope(resnet_arg_scope(is_training=False)):
+        net_conv2, _ = resnet_v1.resnet_v1(net_conv2,
+                                            self._blocks2[0:cfg.RESNET.FIXED_BLOCKS],
+                                            global_pool=False,
+                                            include_root_block=False,
+                                            reuse=reuse,
+                                            scope=self._prev_scope)
+
     if cfg.RESNET.FIXED_BLOCKS < 3:
       with slim.arg_scope(resnet_arg_scope(is_training=is_training)):
         net_conv, _ = resnet_v1.resnet_v1(net_conv,
@@ -114,18 +122,35 @@ class resnetv1(Network):
                                            reuse=reuse,
                                            scope=self._scope)
 
+        net_conv2, _ = resnet_v1.resnet_v1(net_conv2,
+                                            self._blocks2[cfg.RESNET.FIXED_BLOCKS:-1],
+                                            global_pool=False,
+                                            include_root_block=False,
+                                            reuse=reuse,
+                                            scope=self._prev_scope)
+
     self._layers['head'] = net_conv
+    self._layers['head_prev'] = net_conv2
 
     return net_conv
 
-  def _head_to_tail(self, pool5, is_training, reuse=None):
-    with slim.arg_scope(resnet_arg_scope(is_training=is_training)):
-      fc7, _ = resnet_v1.resnet_v1(pool5,
-                               self._blocks[-1:],
-                               global_pool=False,
-                               include_root_block=False,
-                               reuse=reuse,
-                               scope=self._scope)
+  def _head_to_tail(self, pool5, is_training, reuse=None, prev=False):
+    if not prev:
+      with slim.arg_scope(resnet_arg_scope(is_training=is_training)):
+        fc7, _ = resnet_v1.resnet_v1(pool5,
+                                 self._blocks[-1:],
+                                 global_pool=False,
+                                 include_root_block=False,
+                                 reuse=reuse,
+                                 scope=self._scope)
+    else:
+      with slim.arg_scope(resnet_arg_scope(is_training=is_training)):
+        fc7, _ = resnet_v1.resnet_v1(pool5,
+                                         self._blocks2[-1:],
+                                         global_pool=False,
+                                         include_root_block=False,
+                                         reuse=reuse,
+                                         scope=self._prev_scope)
 
     fc7 = tf.reduce_mean(fc7, axis=[1, 2])
     return fc7
